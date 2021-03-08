55f5367c955b37d7a655954a3057a042d9b1ec0d
alasaidi <47666494+alasaidi@users.noreply.github.com>
2021-03-08 13:29:00 +0100

    initial commit

---
+++ Assets/Survival Game Template/Changescene.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+public class Changescene : MonoBehaviour
+{
+    Canvas canvas;
+
+   
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+    public void changeScene(int changetoscene)
+
+    { 
+            SceneManager.LoadScene(changetoscene) ;
+        
+
+    }
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Forest");
+    }
+  
+}
---
+++ Assets/Survival Game Template/Models/Wolf/Anim/IdleState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IdleState : StateMachineBehaviour {
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetFloat("IdleIndex", Random.value);
+    }
+}
